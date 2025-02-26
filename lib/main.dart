8aa8c52fd826e55a6b6a9ef5b3993a2641a454a4
AMANDA_PAOLA_RAMIREZ_AMAYA <a.22308051281284@cbtis128.edu.mx>
2025-02-26 20:09:16 +0000

    paola ramirez

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,57 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconosApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconosApp extends StatelessWidget {
+  const MisIconosApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Paola Ramirez"),
+            titleTextStyle: const TextStyle(color: Colors.black, fontSize: 20),
+            centerTitle: true,
+            backgroundColor: const Color(0xffbfbbbb),
+          ),
+          body: const Column(
+            children: <Widget>[
+              Text(
+                'Paola Ramirez Mat:22308051281284',
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Icon(
+                    Icons.thumb_down_alt_sharp,
+                    color: Colors.red,
+                    size: 40.0,
+                    semanticLabel: 'Text to announce in accessibility modes',
+                  ),
+                  Icon(
+                    Icons.audiotrack,
+                    color: Colors.indigo,
+                    size: 40.0,
+                  ),
+                  Icon(
+                    Icons.beach_access,
+                    color: Colors.blue,
+                    size: 40.0,
+                  ),
+                  Icon(
+                    Icons.add_location,
+                    color: Colors.red,
+                    size: 40.0,
+                  ),
+                ],
+              ),
+            ],
+          )),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
+  } // fin widgets
+} // fin clase MisIconosApp
